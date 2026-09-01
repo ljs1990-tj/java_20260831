6f1ebfa200442f5985815e7f43324f2ea459cf6b
TJ-BU-708-강사PC <TJ-BU-708-PC@TJ-BU-708-Teacher-PC>
2026-09-01 14:29:12 +0900

    20260901

---
+++ FirstProject/src/day2/_10_스캐너.java
@@ -0,0 +1,15 @@
+package day2;
+
+import java.util.Scanner;
+
+public class _10_스캐너 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Ctrl + Shift + O(알파벳)
+		Scanner scan = new Scanner(System.in);
+		
+		
+	}
+
+}
