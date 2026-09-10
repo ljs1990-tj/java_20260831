a729dec3832a7fc969c9226ebcf4c0bb7bada33a
TJ-BU-708-강사PC <TJ-BU-708-PC@TJ-BU-708-Teacher-PC>
2026-09-10 15:19:25 +0900

    2026.09.10

---
+++ FirstProject/src/day9/_8_과일가게수정1.java
@@ -0,0 +1,22 @@
+package day9;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class _8_과일가게수정1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+		System.out.println("====== 과일 가게 프로그램 ======");
+		
+		while(true) {
+			System.out.print("[ (1) 과일등록 (2) 가격수정 (3) 미정 (그 외) 종료 ] : ");
+			int menu = s.nextInt();
+			if(menu == 1) {}
+		}
+	}
+
+}
