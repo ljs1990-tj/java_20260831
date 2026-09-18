47fa8ea5b565768bf84049cb6fe7737262e03047
tjoeun <tjoeun@TJ-BU-708-Teacher-PC>
2026-09-18 11:58:38 +0900

    2026.09.18

--- FirstProject/src/day15/Human.java
+++ FirstProject/src/day15/Human.java
@@ -6,6 +6,12 @@ public class Human {
 	private String gender;
 	String addr;
 	
+	static String intro = "Human 클래스 멤버입니다.";
+	static void intro() {
+//		addr = "인천"; non-static에 접근 불가능
+		System.out.println("Human 클래스 멤버(메소드) 입니다.");
+	}
+	
 	public Human() {};
 	
 	public Human(String name, int age) {
@@ -24,6 +30,34 @@ public class Human {
 	}
 	public String eat(String food) {
 		return food + "를 먹는다.";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if(age < 0) {
+			System.out.println("나이는 0이상 입력해주세요");
+			return;
+		}
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 	
 	
