d0e6375ec921b7c8be46367b99eba95f7c2d8390
tjoeun <tjoeun@TJ-BU-708-Teacher-PC>
2026-09-21 13:07:57 +0900

    2026.09.21

--- FirstProject/src/day16/BlockBinaryCopyEx.java
+++ FirstProject/src/day16/BlockBinaryCopyEx.java
@@ -5,28 +5,31 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class BinaryCopyEx {
+public class BlockBinaryCopyEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double beforeTime = System.currentTimeMillis();
 		File src = new File("C:\\Users\\tjoeu\\OneDrive\\바탕 화면\\image.jpg");
 		File dest = new File("C:\\Users\\tjoeu\\OneDrive\\바탕 화면\\copyimg.jpg");
-		int c;
+		double beforeTime = System.currentTimeMillis(); 
 		try {
 			FileInputStream fi = new FileInputStream(src);
 			FileOutputStream fo = new FileOutputStream(dest);
-			while ((c = fi.read()) != -1) {
-				fo.write((byte) c);
+			byte[] buf = new byte[1024 * 10]; 
+			while (true) {
+				int n = fi.read(buf); 
+				fo.write(buf, 0, n); 
+				if (n < buf.length)
+					break;
 			}
 			fi.close();
 			fo.close();
 			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
 		} catch (IOException e) {
-			// TODO: handle exception
+			System.out.println("파일 복사 오류");
 		}
-		double afterTime = System.currentTimeMillis();
-		double diffTime = (afterTime - beforeTime) / 1000;
+		double afterTime = System.currentTimeMillis(); 
+		double diffTime = (afterTime - beforeTime) / 1000; 
 		System.out.println("시간차이 : " + diffTime);
 	}
 }
