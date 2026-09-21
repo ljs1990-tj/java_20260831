2d8c4212418c78c13b7ab1cdcf87140a402ddd29
tjoeun <tjoeun@TJ-BU-708-Teacher-PC>
2026-09-21 12:57:56 +0900

    2026.09.21

---
+++ FirstProject/src/day16/BinaryCopyEx.java
@@ -0,0 +1,28 @@
+package day16;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BinaryCopyEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File src = new File("C:\\Users\\tjoeu\\OneDrive\\바탕 화면\\image.jpg");
+		File dest = new File("C:\\Users\\tjoeu\\OneDrive\\바탕 화면\\copyimg.jpg");
+		int c;
+		try {
+			FileInputStream fi = new FileInputStream(src);
+			FileOutputStream fo = new FileOutputStream(dest);
+			while ((c = fi.read()) != -1) {
+				fo.write((byte) c);
+			}
+			fi.close();
+			fo.close();
+			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+}
